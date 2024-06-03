7d2b57e7bf17ad4a8cd012ea5f239cb05a096621
Shabanaasgar1997 <shabana.webdeveloper@gmail.com>
2024-06-03 16:57:26 +0530

    Modified the appjs file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import Navbar from "./Navbar";
 import Virat from "./Components/Banner";
 
 function App() {
-  return <h1>HEY</h1>;
+  return (
+    <>
+      <h1>HEY THIS IS FIRST PROJECT DEPLOYED</h1>
+    </>
+  );
 }
 
 export default App;
